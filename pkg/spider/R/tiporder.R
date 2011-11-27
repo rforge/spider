dfef441e6c86c69943c139fc5f36e105077320c6
s_d_j_brown <s_d_j_brown@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-27 07:49:58 +0000

    tiporder modified and website updated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spider@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/spider/R/tiporder.R
+++ pkg/spider/R/tiporder.R
@@ -1,6 +1,6 @@
-tiporder <- function(phy){
+tiporder <- function(phy, labels = TRUE){
 	nn <- length(phy$tip.label) #How many tips on the tree?
 	edge <- phy$edge
 	nums <- rev(edge[edge[,2] %in% 1:nn, 2])
-	phy$tip.label[nums]
+	if(labels == TRUE) phy$tip.label[nums] else nums
 }
--- www/SpiderWebSite/spider_files/spider.js
+++ www/SpiderWebSite/spider_files/spider.js
@@ -1,16 +1,34 @@
-// Created by iWeb 3.0.4 local-build-20111107
+// Created by iWeb 3.0.4 local-build-20111123
+
+//~ function writeMovie1()
+//~ {detectBrowser();if(windowsInternetExplorer)
+//~ {document.write('<object id="id4" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="391" height="260" style="height: 244px; left: 32px; position: absolute; top: 28px; width: 391px; z-index: 1; "><param name="src" value="Media/Spider3-Medium_2.flv" /><param name="controller" value="false" /><param name="autoplay" value="true" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="true" /></object>');}
+//~ else if(isiPhone)
+//~ {document.write('<object id="id4" type="video/quicktime" width="391" height="260" style="height: 244px; left: 32px; position: absolute; top: 28px; width: 391px; z-index: 1; "><param name="src" value="spider_files/Spider3-Medium_2.jpg"/><param name="target" value="myself"/><param name="href" value="../Media/Spider3-Medium_2.flv"/><param name="controller" value="false"/><param name="scale" value="tofit"/></object>');}
+//~ else
+//~ {document.write('<object id="id4" type="video/quicktime" width="391" height="260" data="Media/Spider3-Medium_2.flv" style="height: 244px; left: 32px; position: absolute; top: 28px; width: 391px; z-index: 1; "><param name="src" value="Media/Spider3-Medium_2.flv"/><param name="controller" value="false"/><param name="autoplay" value="true"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="true"/></object>');}}
+//~ setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+//~ {return false;}
+//~ function onPageLoad()
+//~ {loadMozillaCSS('spider_files/spiderMoz.css')
+//~ adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id5');performPostEffectsFixups()}
+//~ function onPageUnload()
+//~ {Widget.onunload();}
+
+
+// To insert Rupert's flash file.
 
 function writeMovie1()
 {detectBrowser();if(windowsInternetExplorer)
-{document.write('<object id="id4" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="387" height="258" style="height: 242px; left: 34px; position: absolute; top: 45px; width: 387px; z-index: 1; "><param name="src" value="Media/Spider3-Large.m4v" /><param name="controller" value="false" /><param name="autoplay" value="true" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="true" /></object>');}
+{document.write('<object id="id4" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="391" height="260" style="height: 244px; left: 32px; position: absolute; top: 28px; width: 391px; z-index: 1; "><param name="src" value="web.swf" /><param name="controller" value="false" /><param name="autoplay" value="true" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="true" /></object>');}
 else if(isiPhone)
-{document.write('<object id="id4" type="video/quicktime" width="387" height="258" style="height: 242px; left: 34px; position: absolute; top: 45px; width: 387px; z-index: 1; "><param name="src" value="spider_files/Spider3-Large.jpg"/><param name="target" value="myself"/><param name="href" value="../Media/Spider3-Large.m4v"/><param name="controller" value="false"/><param name="scale" value="tofit"/></object>');}
+{document.write('<object id="id4" type="application/x-shockwave-flash" width="391" height="260" style="height: 244px; left: 32px; position: absolute; top: 28px; width: 391px; z-index: 1; "><param name="src" value="spider_files/Spider3-Medium_2.jpg"/><param name="target" value="myself"/><param name="href" value="../Media/web.swf"/><param name="controller" value="false"/><param name="scale" value="tofit"/></object>');}
 else
-{document.write('<object id="id4" type="video/quicktime" width="387" height="258" data="Media/Spider3-Large.m4v" style="height: 242px; left: 34px; position: absolute; top: 45px; width: 387px; z-index: 1; "><param name="src" value="Media/Spider3-Large.m4v"/><param name="controller" value="false"/><param name="autoplay" value="true"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="true"/></object>');}}
+{document.write('<object id="id4" type="application/x-shockwave-flash" width="391" height="260" data="Media/web.swf" style="height: 244px; left: 32px; position: absolute; top: 28px; width: 391px; z-index: 1; "><param name="src" value="Media/web.swf"/><param name="controller" value="false"/><param name="autoplay" value="true"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="true"/></object>');}}
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('spider_files/spiderMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id5');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id5');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
