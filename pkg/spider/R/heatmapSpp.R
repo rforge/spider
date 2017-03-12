0e3456824dbee1bc94a27bf52b07014366d4747b
s_d_j_brown <s_d_j_brown@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-12 11:05:53 +0000

    heatmapSpp with extra option added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spider@61 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/spider/R/heatmapSpp.R
+++ pkg/spider/R/heatmapSpp.R
@@ -1,4 +1,4 @@
-heatmapSpp <- function(distObj, sppVector, col = NULL, axisLabels = NULL, triangle = "both"){
+heatmapSpp <- function(distObj, sppVector, col = NULL, axisLabels = NULL, triangle = "both", showData = FALSE, dataRound = 3){
 	if (!is.matrix(distObj)) distObj <- as.matrix(distObj)
 	
 	if (is.null(col)) cols <- c("#D33F6A", "#D95260", "#DE6355", "#E27449", "#E6833D", "#E89331", "#E9A229", "#EAB12A", "#E9C037", "#E7CE4C", "#E4DC68", "#E2E6BD") else cols <- col
@@ -11,6 +11,14 @@ heatmapSpp <- function(distObj, sppVector, col = NULL, axisLabels = NULL, triang
 	if(triangle == "upper") finalMat[lower.tri(sortedMat)] <- NA
 		
 	image(finalMat, col = cols, xaxt = "n", yaxt = "n")
+	
+	if(showData == TRUE){
+		xind <- rep(1:ncol(finalMat), rep(ncol(finalMat), nrow(finalMat)))
+		yind <- rep(1:nrow(finalMat), ncol(finalMat))
+
+		text((xind - 1)/(ncol(finalMat) - 1), (yind - 1)/(nrow(finalMat) - 1), label = mapply(function(x, y) signif(finalMat[x,y], dataRound), x = xind, y = yind))
+	}
+	
 	axis(1, at = seq(0, 1, length.out = dim(distObj)[1]), labels = axisLabels, las = 2)
 	axis(2, at = seq(0, 1, length.out = dim(distObj)[1]), labels = axisLabels, las = 2)
 
