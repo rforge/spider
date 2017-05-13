df6d2991bf21f0b03e1acae45503d3ec59ef3d24
s_d_j_brown <s_d_j_brown@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-05-13 10:08:37 +0000

    Bug fix to seqStat (Thank you RAC!)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spider@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/spider/R/seqStat.R
+++ pkg/spider/R/seqStat.R
@@ -1,7 +1,9 @@
 seqStat <- function(DNAbin, thresh = 500){
-	rr <- sapply(DNAbin, FUN = function(x) length(x) - checkDNA(x, gapsAsMissing=TRUE))
-	tab <- table(NULL)
-	tab[1:5] <- c(min(rr), max(rr), mean(rr), median(rr), length(which(rr < thresh)))
-	names(tab) <- c("Min", "Max", "Mean", "Median", "Thresh")
-	round(tab, digits=0)
+    cd <- checkDNA(DNAbin, gapsAsMissing = TRUE)
+    rr <- sapply(DNAbin, length) - cd
+    tab <- table(NULL)
+    tab[1:5] <- c(min(rr), max(rr), mean(rr), median(rr), length(which(rr < thresh)))
+    names(tab) <- c("Min", "Max", "Mean", "Median", "Thresh")
+    tab <- round(tab, digits = 0)
+    return(tab)
 }
